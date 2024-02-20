bdc83d1a4977ce8dbf4552ab50707e24bb5f8088
Aina <abrenes@nigul.cide.es>
2024-02-20 19:21:32 +0100

    actividad2

---
+++ actividad2/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Table from './componentes/Row';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Llista de compres</h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ actividad2/src/componentes/Row.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+function Table(){
+    return<></>;
+    interface Prompts{
+        lista:{
+            producto:String,
+            cantidad:number,
+            precio:number,
+            tienda:String,
+            notas:String,
+            comprado:Boolean,
+            imagen: String,
+        }
+    
+        };
+    }
+
+
+export default Table;
