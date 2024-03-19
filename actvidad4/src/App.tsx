c8906a51f28c1e68d652a8ac5a548565fb2ce0e7
Aina <abrenes@nigul.cide.es>
2024-03-19 19:16:53 +0100

    actividad

---
+++ actvidad4/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Breeds from './views/Breeds';
+import Razas from './views/Breeds';
+function App() {
+  return (
+    <div>
+ <Razas></Razas>
+  </div>
+  );
+}
+
+export default App;
