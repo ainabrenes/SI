3e42fe1e3fa422e8b57995c605fa1617c6f04f9c
Aina <abrenes@nigul.cide.es>
2024-04-11 16:52:18 +0200

    actividad4

--- actividad5/src/App.tsx
+++ actividad5/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Razas from './views/Breeds';
-import Esp from './views/Esp';
+
 function App() {
   return (
     <BrowserRouter>
